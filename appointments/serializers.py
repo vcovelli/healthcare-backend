69e753bca068cb057c3d2005b68683d50f8b8c03
vcovelli <vincent.covelli78@gmail.com>
2025-01-12 18:12:40 -0500

    Added date/time validation

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Appointment
+from django.utils import timezone
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = '__all__'
+
+    def validate_date_time(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Date and time must be in the future.")
+        return value        
