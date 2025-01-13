3a94d67fb5cd66695bdfa8f4de44745b0943840e
vcovelli <vincent.covelli78@gmail.com>
2025-01-12 22:25:12 -0500

    Update URL configuration to route appointments/ to AppointmentListCreateView for GET and POST requests

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import AppointmentListCreateView
 
 urlpatterns = [
-    path("", AppointmentListCreateView.as_view(), name="appointment_list_create"),
+    path("appointments/", AppointmentListCreateView.as_view(), name="appointment_list_create"),
 ]
