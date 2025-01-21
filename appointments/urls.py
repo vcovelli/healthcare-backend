d2e3c7dcdf820dfbaf18176d01d39ae889d4f391
vcovelli <vincent.covelli78@gmail.com>
2025-01-21 12:24:51 -0500

    Updated URL patterns to include Firebase authentication endpoints

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AppointmentListCreateView, AppointmentDetailView, CreateProfileView
+from .views import AppointmentListCreateView, AppointmentDetailView, CreateProfileView, ProfileView
 
 urlpatterns = [
     path('appointments/', AppointmentListCreateView.as_view(), name='appointment-list-create'),
     path('appointments/<int:pk>/', AppointmentDetailView.as_view(), name='appointment-detail'),
-    path('profiles/', CreateProfileView.as_view(), name='create-profile'),
+    path('profiles/', ProfileView.as_view(), name='profile'),
+    path('profiles/create/', CreateProfileView.as_view(), name='create-profile'),
 ]
