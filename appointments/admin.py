bc4b97254a1b71606369ff6e6af431857f8f74e3
vcovelli <vincent.covelli78@gmail.com>
2025-01-23 02:22:46 -0500

    Removed appointments app files, including models, serializers, views, and migrations, for restructuring

--- appointments/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Appointment
-
-admin.site.register(Profile)
-admin.site.register(Appointment)
--- appointments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AppointmentListCreateView, AppointmentDetailView, CreateProfileView, ProfileView
-
-urlpatterns = [
-    path('appointments/', AppointmentListCreateView.as_view(), name='appointment-list-create'),
-    path('appointments/<int:pk>/', AppointmentDetailView.as_view(), name='appointment-detail'),
-    path('profiles/', ProfileView.as_view(), name='profile'),
-    path('profiles/create/', CreateProfileView.as_view(), name='create-profile'),
-]
