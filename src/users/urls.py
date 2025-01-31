43ee4928f9a03a3cfa02c7a08fb24275955aacac
vcovelli <vincent.covelli78@gmail.com>
2025-01-30 23:03:41 -0500

    Changed and simplified backend URLs.

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,7 +3,6 @@ from src.users.views import ProfileView, UpdateProfileView, CreateProfileView
 
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
-    path("profile/update/<str:pk>", UpdateProfileView.as_view(), name="update-profile"),  # Allow updating profile
-    path("profile/update/", UpdateProfileView.as_view(), name="update-profile-self"),
+    path("profile/update/", UpdateProfileView.as_view(), name="update-profile"),  # Allow updating profile
     path("profile/create/", CreateProfileView.as_view(), name="create-profile"),
 ]
