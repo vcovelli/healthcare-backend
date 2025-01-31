735c5fbc893addb0829e6b13a8343032980b7890
vcovelli <vincent.covelli78@gmail.com>
2025-01-31 00:59:48 -0500

    Conformed database to work with front and backend, changed debugging = false.

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tf^(ac0%k#unr68s&57)i!^y%0r!n01628v)+p7+yegw4*2%u*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['healthcare-backend-0092.onrender.com', '127.0.0.1', 'localhost']
 
